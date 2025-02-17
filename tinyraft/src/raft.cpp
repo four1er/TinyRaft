09206d5f1313e5aa1e1e88e75014143ec52a0dcd
forytian <forytian@tencent.com>
2025-02-17 12:55:14 +0000

    add basic state

--- tinyraft/src/raft.cpp
+++ tinyraft/src/raft.cpp
@@ -1,1 +1,34 @@
 #include "raft.h"
+#include "state.h"
+#include "timer.h"
+
+namespace tinyraft {
+
+void Raft::ProcessTimeout(TimePoint now_time) {
+  switch (current_role_) {
+  case RoleState::FOLLOWER:
+    FollowerTimeout(now_time);
+    break;
+  case RoleState::CANDIDATE:
+    CandidateTimeout(now_time);
+    break;
+  case RoleState::LEADER:
+    LeaderTimeout(now_time);
+    break;
+  default:
+    break;
+  }
+}
+
+void Raft::FollowerTimeout(TimePoint now_time) {}
+
+void Raft::CandidateTimeout(TimePoint now_time) {}
+
+void Raft::LeaderTimeout(TimePoint now_time) {}
+
+void Raft::MakeElection() {}
+
+void Raft::BecomeFollower() { current_role_ = RoleState::FOLLOWER; }
+void Raft::BecomeCandidate() { current_role_ = RoleState::CANDIDATE; }
+void Raft::BecomeLeader() { current_role_ = RoleState::LEADER; }
+} // namespace tinyraft
--- tinyraft/src/raft.h
+++ tinyraft/src/raft.h
@@ -2,6 +2,7 @@
 
 #include "node.h"
 #include "state.h"
+#include "timer.h"
 #include <cstdint>
 #include <memory>
 #include <vector>
@@ -10,7 +11,16 @@ namespace tinyraft {
 class Raft {
 
 public:
-  void ProcessTimeout();
+  /**
+   * @brief ProcessTimeout. We need to identify different role, when happened
+   * timeout.
+   * - Follower timeout: Become candidate and start election \n
+   * - Candidate timeout: Start election again. \n
+   * - Leader timeout: Check heartbeat timeout and send heartbeat \n
+   *
+   * @param [in] now_time
+   */
+  void ProcessTimeout(TimePoint now_time);
 
 private:
   uint64_t id_;                  // node id
@@ -20,5 +30,15 @@ private:
   uint64_t approved_vote_count_; // approved vote count
   TimerState timer_state_;       // timer state
   std::unordered_map<uint64_t, std::shared_ptr<NodeBase>> nodes_maps_;
+
+  void FollowerTimeout(TimePoint now_time);
+  void CandidateTimeout(TimePoint now_time);
+  void LeaderTimeout(TimePoint now_time);
+
+  void MakeElection();
+
+  void BecomeFollower();
+  void BecomeCandidate();
+  void BecomeLeader();
 };
 } // namespace tinyraft
