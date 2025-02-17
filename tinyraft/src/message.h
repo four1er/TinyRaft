b59659a848ce4ee873deec89e66fd8fedac79e15
forytian <forytian@tencent.com>
2025-02-17 11:50:40 +0000

    add example

--- tinyraft/src/message.h
+++ tinyraft/src/message.h
@@ -1,6 +1,20 @@
 #pragma once
 
 #include <cstdint>
-enum MessageType : uint32_t {
 
+/**
+ * @brief different message type
+ *
+ */
+enum MessageType : uint32_t {
+  NONE = 0,
+  LOG_ENTRY = 1,
+  REQUEST_VOTE_REQUEST = 2,
+  REQUEST_VOTE_RESPONSE = 3,
+  APPEND_ENTRIES_REQUEST = 4,
+  APPEND_ENTRIES_RESPONSE = 5,
+  INSTALL_SNAPSHOT_REQUEST = 6,
+  INSTALL_SNAPSHOT_RESPONSE = 7,
+  COMMAND_REQUEST = 8,
+  COMMAND_RESPONSE = 9,
 };
