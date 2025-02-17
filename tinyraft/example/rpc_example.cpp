fca86a05fd1303bfa4a403ff5f92314cfd251266
forytian <forytian@tencent.com>
2025-02-17 11:17:35 +0000

    import yltlibs

---
+++ tinyraft/example/rpc_example.cpp
@@ -0,0 +1,11 @@
+#include "rpc_example.h"
+#include <ylt/coro_rpc/coro_rpc_server.hpp>
+
+using namespace coro_rpc;
+
+int main() {
+  coro_rpc_server server(10, 9000);
+  server.register_handler<echo>();
+  server.start();
+  return 0;
+}
---
+++ tinyraft/example/rpc_example.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <string_view>
+inline std::string_view echo(std::string_view str) { return str; }
